d3b82183925b5ec151fc1eed11dc1b17cd137414
Kristo <mikkonek@gmail.com>
2016-09-23 11:33:23 +0100

    Added first tests for Node Revel Constructor.

---
+++ test/tests.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const NodeRevel = require('../lib/NodeRevel.js');
+const chai = require('chai');
+const expect = chai.expect;
+// const chaiAsPromised = require('chai-as-promised');
+// const sinon = require('sinon');
+// require('sinon-as-promised');
+
+// chai.use(chaiAsPromised);
+
+describe('Testing the constructer of the Revel Class', () => {
+
+  var endpoint = 'https://xxxxxxx.revelup.com';
+  var apiKey = 'XXXXXXXXXXXXXXXXXX';
+  var apiSecret = 'xxxxxxxxxxxxxxxxx';
+  var enterpriseUserUri = '/enterprise/User/8/';
+
+
+  it('Should be an instance of NodeRevel', () => {
+
+    const nodeRevel = new NodeRevel(endpoint, apiKey, apiSecret, enterpriseUserUri);
+    expect(nodeRevel).to.be.a.instanceof(NodeRevel);
+
+  });
+
+
+});
